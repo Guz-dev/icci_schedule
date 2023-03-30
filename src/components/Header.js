86d6c4db98a9964d5a356a8839f25de5a3449277
danyel3678 <76797363+danyel3678@users.noreply.github.com>
2023-03-30 20:51:01 -0300

    Componentes

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+export default function Header() {
+  return (
+    <>
+      <div className="grid grid-cols-6">
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+        <div className="bg-gray-100 border border-red-500">01</div>
+
+      </div>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
+import Header from '../components/Header'
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
+
         <Head>
-          <title>Icci_schedule - Home</title>
+          <Header></Header>
         </Head>
         <h1>Test App</h1>
       </div>
