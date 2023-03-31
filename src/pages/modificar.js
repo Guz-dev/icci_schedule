85019692e66bcba5307380e7b6f8496944722a92
danyel3678 <76797363+danyel3678@users.noreply.github.com>
2023-03-31 20:14:40 -0300

    Componente modificar

---
+++ src/pages/modificar.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Modificar from "../components/Modificar";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.container}>
+
+        <Head>
+        </Head>
+        <Modificar></Modificar>
+        <h1>MODIFICAR</h1>
+      </div>
+    </>
+  );
+}
