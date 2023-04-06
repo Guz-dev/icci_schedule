de8219834c3657173044d573fc57476be68f4f6b
Gustavo <tavo.3900@gmail.com>
2023-04-05 20:58:47 -0400

    Update semestre_api.js

--- src/pages/api/semestre_api.js
+++ src/pages/api/semestre_api.js
@@ -1,9 +1,15 @@
 import { supabase } from "./supabaseClient"
 
 export default async function handler(req, res) {  
-  
   const { data } = await supabase.from('semestres')
-              .select(`semestre,  bloques_horario( profesor )`)//.order('id_ramos', { ascending: true }
+              .select(`semestre, 
+              bloques_horario( profesor, sala, grupo, ramos( ramo ))
+              `)
+  /* 
+  const { data } = await supabase.from('semestres')
+              .select(`semestre, 
+              bloques_horario( profesor, sala, grupo, ramos(ramo)) `) *///.order('id_ramos', { ascending: true }
+              //.order('id_ramos', { ascending: true }
   /*
   const { data } = await supabase.from('ramos')
               .select("*")//.order('id_ramos', { ascending: true }`)
