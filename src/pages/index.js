d29f1a3249f9c0bdd96829ffad20415ef11ee8b5
Gustavo <tavo.3900@gmail.com>
2023-04-10 18:50:19 -0400

    Componente bloque horario testeo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,15 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import Bloque_horario from "components/bloque_horario";
 
 export default function Home({ bloques }) {
 
   console.log(bloques);
   const semSym = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI']
   const tablTh = ['Clave Hora','Lunes','Martes','Miercoles','Jueves','Viernes']
-  const hora = ['8:00','9:40','11:20','14:45','16:20','17:55','19:30']
-  const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+  const horas = [['8:00','9:30'],['9:40','11:10'],['11:20','12:50'],['14:45','16:10'],['16:20','17:50'],['17:55','19:25'],['19:30','21:00']]
+  const dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
 
   const [semestre,setSemestre] = useState(1)
 
@@ -55,73 +56,58 @@ export default function Home({ bloques }) {
                   })}
                   </tr>
                   </thead>
-                  <tbody class=""> 
-                  {bloques.map((bloque,index)=>{
-                    if (bloque.semestre===semestre) {
-                      return (
-                        <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-gray-200">
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.bloques_horas.inicio} - {bloque.bloques_horario.bloques_horas.termino}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.dia === 'Lunes' && (
-                              <div>
-                                <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
-                                <div className="text-gray-500">
-                                  {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
-                                  {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
-                                </div>
-                              </div>
-                            )}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.dia === 'Martes' && (
-                              <div>
-                              <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
-                              <div className="text-gray-500">
-                                {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
-                                {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
-                              </div>
-                            </div>
-                            )}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.dia === 'Miercoles' && (
-                              <div>
-                              <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
-                              <div className="text-gray-500">
-                                {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
-                                {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
-                              </div>
-                            </div>
-                            )}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.dia === 'Jueves' && (
-                              <div>
-                              <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
-                              <div className="text-gray-500">
-                                {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
-                                {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
-                              </div>
-                            </div>
-                            )}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
-                            {bloque.bloques_horario.dia === 'Viernes' && (
-                              <div>
-                              <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
-                              <div className="text-gray-500">
-                                {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
-                                {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
-                              </div>
-                            </div>
-                            )}
-                          </td>
-                        </tr>
-                      )
-                    }
+                  <tbody class="">
+
+
+
+                  {horas.map((hora,index) => {
+                    return(
+                      <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-gray-200">
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r border-solid border-gray-700">
+                          {hora[0]} - {hora[1]}
+                        </td>
+
+                        {dias.map((dia,index) => {
+                          return(
+                            <td key={index} className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
+                              <Bloque_horario params={{semestre,hora,dia}}/>
+                            </td>
+                          )
+                        })}
+
+
+                      </tr>
+                    )
                   })}
+
+                  {/* {horas.map((hora,index) => {
+                    var j = 0
+                    return(
+                      <tr key={index} className="border-b transition duration-300 ease-in-out hover:bg-gray-200">
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r border-solid border-gray-700">
+                          {hora} - {hora}
+                        </td>
+                        {bloques.map((bloque,index)=>{
+                          if (bloque.semestre===semestre) {
+                            return (                                                                                     
+                              <td key={index} className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r border-solid border-gray-700">
+                                {bloque.bloques_horario.dia == dias[j] && (
+                                <div>
+                                  <div className="font-medium">{bloque.bloques_horario.ramos.ramo}</div>
+                                  <div className="text-gray-500">
+                                    {bloque.bloques_horario.sala} - Grupo {bloque.bloques_horario.grupo}<br></br>
+                                    {bloque.bloques_horario.profesor} {bloque.bloques_horario.dia}
+                                  </div>
+                                </div>
+                                )}
+                              </td>                            
+                            )
+                          }
+                          j = j + 1
+                        })}
+                      </tr>
+                    )})} */}
+
                   </tbody>
                 </table>
               </div>
