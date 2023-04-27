04f09519481507f67449b8553e4a79ac6f9ef83c
Darchygithub <92606417+Darchygithub@users.noreply.github.com>
2023-04-26 20:18:08 -0400

    header actualizado app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,17 +14,18 @@ export default function App({ Component, pageProps }) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
       
-        <header class="flex justify-between flex-wrap bg-[#17286b] p-1 px-4">
+        <header class="flex md:justify-around md:flex-wrap md:flex-row bg-[#17286b] p-1 px-4 justify-evenly flex-col flex-wrap items-center">
           <div class="flex items-center flex-shrink-0 text-white mr-6">
             <Link href=".">
-              <Image className="py-3.5 pr-2" src="/logo_web_uta.png" alt="Logo alt UTA" height={300} width={400}></Image>
+              <Image className="py-3.5 pr-2 mx-6" src="/logo_web_uta.png" alt="Logo alt UTA" height={300} width={400}></Image>
             </Link>
-            <div class="mx-auto flex w-[200px] px-6 "> ING. COMPUTACIÓN E INFORMÁTICA AÑO 2023 </div>
+            <div class="mx-auto flex w-[225px] px-6  text-center "> ING. COMPUTACIÓN E INFORMÁTICA AÑO 2023 </div>
           </div> 
-          <div class="flex items-center text-white mr-6">    
-            <span class="font-semibold self-center text-xl uppercase">📅 Icci_schedule</span>
+          <div class="flex items-center text-white mr-6 ">    
+            <div class="font-semibold self-center uppercase lg:inline-flex flex-col text-3xl ">📅 Horario de clases ICCI</div>
           </div>
-          <Link href='login' class="px-4 py-2 self-center text-white hover:text-amber-300 hover:delay-75 hover:border-yellow-300 lg:mt-0 rounded-full border-2 border-amber-300 ">MODIFICAR</Link>
+          <Link href='login' class="px-4 py-2 self-center text-white hover:text-amber-300 hover:delay-75 hover:border-yellow-300 lg:mt-0 rounded-full border-2 border-amber-300">MODIFICAR</Link>
+          {/* <Link href='login' class="px-4 py-2 self-center text-white hover:text-amber-300 hover:delay-75 hover:border-yellow-300 lg:mt-0 rounded-full border-2 border-amber-300 xl:text-red-600 lg:text-green-600 md:text-cyan-600 sm:text-orange-600 visible xl:hidden"> Prueba </Link> */}
         </header>
         <Component {...pageProps} />      
       </ModalProvider>
