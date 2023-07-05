0f64b476e42fa59ac48001f646a68df8775ebb4c
Gustavo <tavo.3900@gmail.com>
2023-07-05 18:38:55 -0400

    indent

--- src/components/ClaveHora.js
+++ src/components/ClaveHora.js
@@ -1,7 +1,7 @@
 export default function ClaveHora({hora}){
-    return(    
-        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r border-solid border-gray-700">
-            {hora[0]} - {hora[1]}
-        </td>
-    )
+  return(    
+    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r border-solid border-gray-700">
+      {hora[0]} - {hora[1]}
+    </td>
+  )
 }
--- src/components/IndicadorDia.js
+++ src/components/IndicadorDia.js
@@ -3,9 +3,9 @@ export default function IndicadorDia(){
   return(    
     <thead className="bg-[#17286b] border-b border-gray-100 text-white">
       <tr>
-          {tablTh.map((tabl,index) => (
-            <th key={index} scope="col" className="text-sm font-bold px-6 py-4 text-center border-r border-solid">{tabl}</th>
-          ))}
+        {tablTh.map((tabl,index) => (
+          <th key={index} scope="col" className="text-sm font-bold px-6 py-4 text-center border-r border-solid">{tabl}</th>
+        ))}
       </tr>
     </thead>
   )
